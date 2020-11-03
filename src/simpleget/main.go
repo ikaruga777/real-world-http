5cf74d36a9958112e730115b7ffbdd776405e3a8
ikaruga <ikaruga777@gmail.com>
2020-11-03 19:51:27 +0900

    http.Getを使って特定のページ内容を取得するくん

---
+++ src/simpleget/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://ikaruga.org")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+}
